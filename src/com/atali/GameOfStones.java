619a34069af9a577541a3f24e307b6bbc2f5f875
Atali DAOUD <atali.daoud@zenittude.com>
2016-06-11 12:18:47 +0200

    Initial commit

---
+++ src/com/atali/GameOfStones.java
@@ -0,0 +1,42 @@
+package com.atali;
+
+import java.util.Scanner;
+
+public class GameOfStones {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int[] moves = {2,3,5};
+            int currentMove = n;
+
+            int currentPlayer = 0;
+
+            do {
+
+                if( currentMove < 2) {
+                    currentPlayer++;
+                    break;
+                }
+
+                boolean hasMoved = false;
+                for( int index = 0; index < moves.length; index++) {
+                    if( currentMove - moves[index] >= 0 ) {
+                        currentMove -= moves[index];
+                        hasMoved = true;
+                        break;
+                    }
+                }
+
+                if( hasMoved) {
+                    currentPlayer++;
+                }
+
+            } while( true);
+
+            System.out.println(currentPlayer%2 == 0 ? "First":"Second");
+        }
+    }
+}
